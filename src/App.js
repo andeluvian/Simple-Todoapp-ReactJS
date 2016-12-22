6dfaa63ba3e10677e302bfe4914131cd3b411cb0
Ken <kenneth.forsman@oneup.fi>
2016-12-22 17:48:00 +0200

    second commit, AWS connection working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+          super(props);
+
+          this.handleSubmit = this.handleSubmit.bind(this);
+
+      }
+
+      handleSubmit(event) {
+
+          fetch('https://jxmar3hx36.execute-api.eu-west-1.amazonaws.com/test/rng', {
+            method: 'GET',
+            mode: 'cors',
+            headers: new Headers({
+              'Content-Type': 'application/json'
+            })
+
+          }).then(res => res.text()).then((data)=>{
+
+                this.setState({rng: + data})
+                console.log(data);
+              
+          })
+event.preventDefault();
+      }
+
+
   render() {
     return (
       <div className="App">
@@ -13,6 +40,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={this.handleSubmit}>
+            <input type="submit" value="Submit"/>
+        </form>
+
+
       </div>
     );
   }
