1dd518ec65c639080cfc4b902efbc7631b8b73ba
Ken <kenneth.forsman@oneup.fi>
2016-12-27 22:56:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import { hashHistory,browserHistory,Router, Route, Link,IndexRoute } from 'react-router';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './components/Main'
-import Wallball from './components/Wallball'
+
 class App extends Component {
-
-  constructor(props) {
-          super(props);
-
-          this.handleSubmit = this.handleSubmit.bind(this);
-
-      }
-
-      handleSubmit(event) {
-
-          fetch('https://jxmar3hx36.execute-api.eu-west-1.amazonaws.com/test/rng', {
-            method: 'GET',
-            mode: 'cors',
-            headers: new Headers({
-              'Content-Type': 'application/json'
-            })
-
-          }).then(res => res.text()).then((data)=>{
-
-                this.setState({rng: + data})
-                console.log(data);
-
-          })
-event.preventDefault();
-      }
-
-
   render() {
     return (
-  <div className="App">
-
-
-
+      <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -45,18 +13,6 @@ event.preventDefault();
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <form onSubmit={this.handleSubmit}>
-            <input type="submit" value="Submit"/>
-        </form>
-        <Router history={browserHistory}>
-
-             <Route path='/' component={Main}>
-                  <Route path='/Wallball' component={Wallball}></Route>
-
-
-             </Route>
-           </Router>
-
       </div>
     );
   }
